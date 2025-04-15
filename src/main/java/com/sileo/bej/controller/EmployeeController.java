9023a03f9d06b6d732dc6c8e143361d19a1ef8e0
shivam dubey <youremail@example.com>
2025-04-15 15:56:15 +0530

    made a file called  WebConfig.java and removed erlier @CrossOrigin annotation from controller layer

--- src/main/java/com/sileo/bej/controller/EmployeeController.java
+++ src/main/java/com/sileo/bej/controller/EmployeeController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
